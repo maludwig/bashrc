75cf1d175b81738862b74d3159b5c8dee8525634
Mitchell Ludwig <mitchell.ludwig@gmail.com>
2015-06-07 23:35:04 -0600

    Fixed regex.py

--- regex.py
+++ regex.py
@@ -4,16 +4,20 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser(description='Search with Python regular expressions')
-parser.add_argument('-I', '--insensitive', default=False, help="Case insensitive search")
-parser.add_argument('-n', '--noisy', default=False, help="Print only matching input")
+parser.add_argument('-I', '--insensitive', action='store_true', help="Case insensitive search")
+parser.add_argument('-n', '--noisy', action='store_true', help="Print only matching input")
 parser.add_argument('-f', '--file', default=sys.stdin, type=argparse.FileType('r'), help="Input file")
 parser.add_argument('-o', '--output', default=sys.stdout, type=argparse.FileType('w'), help="Output file")
 parser.add_argument('needle',help="Regex to find")
-parser.add_argument('replace',nargs='?',default="",help="(Optional) replacement regex, supports 1 notation")
+parser.add_argument('replace',nargs='?',default="",help="(Optional) replacement regex, supports \\1 notation")
 
 args = parser.parse_args()
 
-print "Needle: |%s|" % args.needle
+sys.stderr.write("Needle: /%s/\n" % args.needle)
+sys.stderr.write("Replace: /%s/\n" % args.replace)
+sys.stderr.write("Insensitive: /%s/\n" % args.insensitive)
+sys.stderr.write("Noisy: /%s/\n" % args.noisy)
+
 if args.insensitive:
 	rneedle = re.compile(args.needle,re.IGNORECASE)
 else:
