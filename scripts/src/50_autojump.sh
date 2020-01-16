6ad9ca67ed2660e74eafd30c50a1033ef2a42c7f
Mitchell Ludwig <mitchell.ludwig@gmail.com>
2020-01-16 11:43:05 -0700

    zsh fixes

--- scripts/src/50_autojump.sh
+++ scripts/src/50_autojump.sh
@@ -1,4 +1,3 @@
-
 if [[ -f "$BASHRC_DIR/modules/autojump/bin/autojump.sh" ]]; then
   AUTOJUMP_SH="$BASHRC_DIR/modules/autojump/bin/autojump.sh"
 elif [[ -f '/usr/share/autojump/autojump.sh' ]]; then
@@ -10,13 +9,11 @@ else
     $BASHRC_DIR/modules/autojump/bin/autojump.sh
   "
 fi
-
 # autojump
 if [[ -f "$AUTOJUMP_SH" ]]; then
   source "$AUTOJUMP_SH"
   export AUTOJUMP_DB=`autojump -s | grep -E '^data:' | sed -E $'s/^data:[\t ]+//'`
 fi
-
 if [[ "$BASH_VERSION" > "4" ]]; then
   shopt -s autocd
 fi
